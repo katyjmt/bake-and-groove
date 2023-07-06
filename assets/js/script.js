20a282b4954752f9fdc1d849d6bd39ed2d62bf92
x.ande <ananfro@live.com>
2023-07-06 13:06:09 +1000

    added a working box effect

---
+++ assets/js/script.js
@@ -0,0 +1,54 @@
+//BOX EFFECT TODO:
+//create separate js file for it
+//make the div absolute so it won't affect the page layout
+//code refactor and clean
+
+const vpHeight = window.innerHeight;
+const vpWidth = window.innerWidth;
+const boxWidth = 75;
+const boxHeight = 75;
+const fadeInitialLevel = 0.5;
+const s_boxEffect = document.querySelector("#boxEffect");
+
+console.log(vpWidth + " x " + vpHeight);
+
+for (i = 0; i < vpWidth * vpHeight; i += boxWidth * boxHeight) {
+
+    let boxEl = document.createElement("div");
+    boxEl.setAttribute("style", `height: ${boxHeight}px; width: ${boxWidth}px; background-color: black; opacity: 0;`);
+    boxEl.setAttribute("class", "boxybox");
+
+    s_boxEffect.append(boxEl);
+
+    console.log(i, vpWidth * vpHeight);
+
+}
+
+const sArray_boxyBox = document.querySelectorAll(".boxybox");
+
+for (i = 0; i < sArray_boxyBox.length; i++) {
+
+    sArray_boxyBox[i].addEventListener("mouseover", function (e) {
+
+        let fadeLevel = fadeInitialLevel;
+        e.target.setAttribute("style", `height: ${boxHeight}px; width: ${boxWidth}px; background-color: black; opacity: ${fadeLevel};`);
+
+        let effectFadeDown = setInterval(function () {
+
+            if (fadeLevel <= 0) {
+
+                clearInterval(effectFadeDown);
+
+            } else if (fadeLevel > 0) {
+
+                fadeLevel = fadeLevel - 0.05;
+                e.target.setAttribute("style", `height: ${boxHeight}px; width: ${boxWidth}px; background-color: black; opacity: ${fadeLevel};`);
+                console.log(fadeLevel);
+
+            }
+
+        }, 50);
+
+    });
+
+}
